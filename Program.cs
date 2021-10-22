104438b88c1a9e4789d2a6196765e8e8e00542ee
Meysam03 <89509192+Meysam03@users.noreply.github.com>
2021-10-22 14:08:42 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace mopa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randNumb = rand.Next(1, 101);
+            int försök = 1;
+
+            while(true){
+                Console.Write("--> Gissa ett tal ; ");
+                string Gissa = Console.ReadLine();
+                int gissa;
+                if(int.TryParse(Gissa, out gissa) == false){
+                    Console.WriteLine("fel!");
+                    continue;
+                }
+
+                if(gissa == randNumb){
+                    break;
+                }
+                else if(gissa > randNumb){
+                    Console.WriteLine("Du gissade för högt!");
+                }
+                else{
+                    Console.WriteLine("Du gissade för lågt!");
+                }
+                försök++;
+            }
+
+            Console.Write("braa! Du gissade rätt. Det tog ");
+            Console.WriteLine(" försök :)");
+            
+            
+
+        }
+    }
+}
